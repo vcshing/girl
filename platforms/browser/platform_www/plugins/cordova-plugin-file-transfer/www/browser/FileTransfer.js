f9d8e98f173ba5b2692be3eec57684c4e0f11b41
Skyler Liu <vcshing2005@hotmail.com>
2016-12-13 21:27:43 +0800

    asd

---
+++ platforms/browser/platform_www/plugins/cordova-plugin-file-transfer/www/browser/FileTransfer.js
@@ -0,0 +1,2 @@
+cordova.define("cordova-plugin-file-transfer.BrowserFileTransfer", function(require, exports, module) { 
+});
---
+++ platforms/browser/platform_www/plugins/cordova-plugin-splashscreen/www/splashscreen.js
@@ -0,0 +1,2 @@
+cordova.define("cordova-plugin-splashscreen.SplashScreen", function(require, exports, module) { 
+});
